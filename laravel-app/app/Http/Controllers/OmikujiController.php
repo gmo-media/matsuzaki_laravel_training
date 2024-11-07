64cad4507d551c1470662388b347cffbff17662a
松崎 歓喜 <kanki.matsuzaki@gmo.media>
2024-11-07 16:42:35 +0900

    コントローラーの処理作成

---
+++ laravel-app/app/Http/Controllers/OmikujiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\OmikujiInterface;
+use Omikuji;
+
+class OmikujiController extends Controller
+{
+    protected OmikujiInterface $omikuji;
+
+    public function __construct(OmikujiInterface $omikuji)
+    {
+        $this->omikuji = $omikuji;
+    }
+
+    public function draw()
+    {
+        $result = Omikuji::draw();
+        return response()->json(['result' => $result], 200, [], JSON_UNESCAPED_UNICODE);
+
+    }
+}
